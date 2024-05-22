f5de61d1f1c9a51dc41f19e85a2209c068ad3cce
IzaeahMyhre <158678029+IzaeahMyhre@users.noreply.github.com>
2024-05-22 00:49:16 -0600

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -40,6 +40,9 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").textContent.firstElementChild;
 
 function closePopop() {
   profileEditModal.classList.remove("modal_opened");
